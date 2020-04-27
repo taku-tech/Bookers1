9aafb2eb2b7ae687196c828b5d5057754499a541
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-04-27 17:21:45 +0900

    [Update]booksルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'homes/top'
   get 'books/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
 end
