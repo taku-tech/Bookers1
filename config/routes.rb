86f2701c3dfb6a9d5ceb5aeb044f1dfd7a97f987
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-04-29 22:02:19 +0900

    [Add]削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'homes/top'
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :create, :show, :edit, :update]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 end
