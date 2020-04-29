8fa9df3d2de77c92ee99aeae53f2cf18b4fae0bc
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-04-29 12:54:32 +0900

    [Remove]newアクションの削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,13 @@
 class BooksController < ApplicationController
   def index
   	@books = Book.all
-  end
-
-  def new
   	@book = Book.new
   end
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+    book.save
+    redirect_to books_path
   end
 
   private
